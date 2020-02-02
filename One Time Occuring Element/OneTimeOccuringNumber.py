a4053aaa4839cdb9a91bf6664fc70ae1b6956478
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2020-02-03 00:14:01 +0530

    Add files via upload

---
+++ One Time Occuring Element/OneTimeOccuringNumber.py
@@ -0,0 +1,45 @@
+# Python3 code to find the element that 
+# appears once 
+
+def getSingle(arr, n): 
+	ones = 0
+	twos = 0
+	
+	for i in range(n): 
+		# one & arr[i]" gives the bits that 
+		# are there in both 'ones' and new 
+		# element from arr[]. We add these 
+		# bits to 'twos' using bitwise OR 
+		twos = twos | (ones & arr[i]) 
+		
+		# one & arr[i]" gives the bits that 
+		# are there in both 'ones' and new 
+		# element from arr[]. We add these 
+		# bits to 'twos' using bitwise OR 
+		ones = ones ^ arr[i] 
+		
+		# The common bits are those bits 
+		# which appear third time. So these 
+		# bits should not be there in both 
+		# 'ones' and 'twos'. common_bit_mask 
+		# contains all these bits as 0, so 
+		# that the bits can be removed from 
+		# 'ones' and 'twos' 
+		common_bit_mask = ~(ones & twos) 
+		
+		# Remove common bits (the bits that 
+		# appear third time) from 'ones' 
+		ones &= common_bit_mask 
+		
+		# Remove common bits (the bits that 
+		# appear third time) from 'twos' 
+		twos &= common_bit_mask 
+	return ones 
+	
+# driver code 
+arr = [0,0,0,1] 
+n = len(arr) 
+print("The element with single occurrence is ", 
+		getSingle(arr, n)) 
+
+# This code is contributed by "Abhishek Sharma 44" 
